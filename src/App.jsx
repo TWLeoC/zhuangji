8eb4f139d0d54ad5bceb172163d2db1875d40701
LEO CHOU <leotimjack@gmail.com>
2026-03-18 21:37:02 +0800

    fix: all.css import loadingpage and delete App.jsx remove import Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { RouterProvider } from 'react-router';
 import { router } from './router';
-import Home from './pages/front/Home/Home';
 
 function App() {
   return <RouterProvider router={router} />;
